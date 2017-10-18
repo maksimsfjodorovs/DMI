e949ababf6b8e3325f9a7c6d80b37d08a0a18703
Guest <fjodorovs65@gmail.com>
2017-10-18 16:08:23 +0300

    20171018_16_10

---
+++ a_VS_b.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Input a:"
+read a
+echo "Input b:"
+read b
+
+#if [ $a -ne $b ]
+#then
+#	if [ $a -gt $b ]
+#	then
+#	echo "a ($a) > b ($b)"
+#	else
+#	echo " a ($a) < b ($b)"
+#	fi
+#else
+#echo " a ($a) is equal b ($b) "
+#fi
+
+#if [ $a -eq $b ]
+if (( $a == $b ))
+then
+echo "a ($a) = b ($b)"
+#elif [ $a -gt $b ]
+elif (( $a > $b ))
+then
+echo " a ($a) > b ($b)"
+else
+echo " a ($a) < b ($b)"
+fi
+
+#if [ $a -gt $b ]
+#then
+#echo "jā a ($a) > b ($b)"
+#else
+#echo "nē a ($a) nav lielāks par b ($b)"
+#else
+#fi
+
+#c=`expr $a + $b`
+#echo "$a + $b = "$c
+
